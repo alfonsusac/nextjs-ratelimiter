42020b67e8d705b681f4032cb98e8b6ff3a065e9
Alfonsus Ardani <alfonsusac@gmail.com>
2023-11-09 22:18:47 +0700

    memoryleak

---
+++ src/app/api/memoryleak/route.ts
@@ -0,0 +1,16 @@
+let obj: {[key: string]:string}  = {}
+
+export function GET() {
+  
+  const len = Object.keys(obj).length + 1
+  for (let i = 0; i < len ; i++){
+    obj[crypto.randomUUID()] = crypto.randomUUID()
+  }
+
+  return Response.json({
+    size: Object.keys(obj).length,
+    obj
+  })
+}
+
+export const dynamic = 'force-dynamic'
--- src/app/api/unstablecache/route.ts
+++ src/app/api/unstablecache/route.ts
@@ -2,7 +2,7 @@ import { unstable_cache } from "next/cache"
 
 const cachedRandomize = unstable_cache(async () => {
   return Math.random()
-})
+},['firstcache'])
 export async function GET() {
   // Rate Limit
   const random = await cachedRandomize()
