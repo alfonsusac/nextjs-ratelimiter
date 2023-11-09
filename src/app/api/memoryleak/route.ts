1b117167ed5b6d4f6020805b52ee7985d1095fbf
Alfonsus Ardani <alfonsusac@gmail.com>
2023-11-09 23:18:36 +0700

    limit at 500k

--- src/app/api/memoryleak/route.ts
+++ src/app/api/memoryleak/route.ts
@@ -1,11 +1,12 @@
-let obj: {[key: string]:string}  = {}
+let obj: { [key: string]: string } = {}
 
 export function GET() {
-  
+
   const len = Object.keys(obj).length + 1
-  for (let i = 0; i < len ; i++){
-    obj[crypto.randomUUID()] = crypto.randomUUID()
-  }
+  if (len < 500000)
+    for (let i = 0; i < len; i++) {
+      obj[crypto.randomUUID()] = crypto.randomUUID()
+    }
 
   return Response.json({
     size: Object.keys(obj).length,
